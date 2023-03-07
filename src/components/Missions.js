da8f44182041b9128610bbf2f0ab171d6d2a71b0
chaseknis <chase6th@gmail.com>
2023-03-07 07:40:34 +0200

    Create basic structure for Missions to resolve #20

---
+++ src/components/Missions.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Missions = () => (
+  <div className="mission_container">
+    <table className="table_container">
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th>Empty Title</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Thaicon</td>
+          <td>dgdfhhgshkfbvfljavbfb kjfbajdfbvusdbsf sufbhalkhgvjadbvjv</td>
+          <td>dgdfhhgshkfbvfljavbfb kjfbajdfbvusdbsf sufbhalkhgvjadbvjv</td>
+          <td>Not a member</td>
+          <td><button type="button">Join Mission</button></td>
+        </tr>
+      </tbody>
+
+    </table>
+
+  </div>
+
+);
+
+export default Missions;
