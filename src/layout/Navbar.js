6dd6838b6b88ea0d1ed73387fb36bd5e99a79b1e
1iceberg1 <aleksander.iceberg1202@gmail.com>
2023-04-14 20:38:21 +0630

    add hamburger menu for media query 768

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,7 +1,13 @@
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 import './styles/navbar.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   const underline = ({ isActive }) => ({
     textDecoration: isActive ? 'underline' : 'none',
   });
@@ -11,11 +17,16 @@ const Navbar = () => {
         <img src={require('../photo/planet.png')} alt="logo" />
         <h1>Space Travelers&apos; Hub</h1>
       </nav>
-      <nav className="navLink">
+      <nav className={`navLink ${isOpen ? 'open' : ''}`}>
         <span><NavLink to="/" style={underline}>Rockets</NavLink></span>
         <span><NavLink to="/missions" style={underline}>Missions</NavLink></span>
         <span><NavLink to="/myprofile" style={underline}>My Profile</NavLink></span>
       </nav>
+      <button type="button" className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <span className="line" />
+        <span className="line" />
+        <span className="line" />
+      </button>
     </nav>
   );
 };
